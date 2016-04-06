05c3f659535f47c851f44ad4883736d39a99c286
Mohit Thatte <mohit.thatte@gmail.com>
2016-04-06 11:09:54 +0530

    Add some nice custom faces to clojure mode

--- personal/init-clj-refactor.el
+++ personal/init-clj-refactor.el
@@ -4,13 +4,43 @@
 ;;; Code:
 (prelude-require-package 'clj-refactor)
 
+(defun pretty-fns ()
+  "fns become ƒ"
+  (font-lock-add-keywords
+   nil `(("(\\(fn\\)[\[[:space:]]"
+          (0 (progn (compose-region (match-beginning 1) (match-end 1)
+                                    "ƒ")
+                    nil))))))
+
+
+(defun pretty-lambdas ()
+  "anon fns become lambda"
+  (font-lock-add-keywords
+   nil `(("\\(#\\)("
+          (0 (progn (compose-region (match-beginning 1) (match-end 1)
+                                    ,(make-char 'greek-iso8859-7 107))
+                    nil))))))
+
+
+(defun pretty-sets ()
+  "sets become ∈"
+  (font-lock-add-keywords
+   nil `(("\\(#\\){"
+          (0 (progn (compose-region (match-beginning 1) (match-end 1)
+                                    "∈")
+                    nil))))))
+
+
 (defun cljr-clojure-mode-hook ()
   "Setup cljr within clojure-mode."
-  (clj-refactor-mode +1)
-  (yas-minor-mode +1)
+  (clj-refactor-mode 1)
+  (yas-minor-mode 1)
   (cljr-add-keybindings-with-prefix "C-c C-m"))
 
-(add-hook 'clojure-mode-hook #'cljr-clojure-mode-hook)
+(add-hook 'clojure-mode-hook 'cljr-clojure-mode-hook)
+(add-hook 'clojure-mode-hook 'pretty-fns)
+(add-hook 'clojure-mode-hook 'pretty-sets)
+(add-hook 'clojure-mode-hook 'pretty-lambdas)
 
 (provide 'init-clj-refactor)
 ;;; init-clj-refactor.el ends here
