b4a9f36954096f832400008d33826b2d77cf9510
Mohit Thatte <mohit.thatte@gmail.com>
2017-02-11 16:37:20 +0530

    web mode and some more indents

--- personal/pastafari.el
+++ personal/pastafari.el
@@ -75,14 +75,16 @@
 (setq web-mode-enable-auto-pairing t)
 (setq web-mode-enable-current-element-highlight t)
 (setq web-mode-enable-current-column-highlight t)
+(setq web-mode-enable-auto-pairing t)
+(setq web-mode-enable-auto-closing t)
 
 ;; json-mode
 (setq json-reformat:indent-width 2)
 
-
 ;; tab width and tab stops
 (setq tab-width 2)
 (setq-default tab-stop-list (number-sequence 2 100 2))
+(setq standard-indent 2)
 
 ;; TODO: cleanup org-mode setup, extract into separate file maybe?
 ;; org-journal
