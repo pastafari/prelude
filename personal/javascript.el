dc778c9eb1187d474185d960e3a8db24d2211954
Mohit Thatte <mohit.thatte@gmail.com>
2020-05-24 16:59:26 +0200

    Add node-modules-path for javascript

--- personal/javascript.el
+++ personal/javascript.el
@@ -1,6 +1,9 @@
 ;; Requires rjsx for React stuff
-
+;; and adds rjsx as a minor mode to *.js
 (prelude-require-package 'rjsx-mode)
-
-;; adds rjsx as a minor mode to *.js
 (add-to-list 'auto-mode-alist '("\\.js\\'" . rjsx-mode))
+
+;; requires add-node-modules-path and adds it to js-mode
+(prelude-require-package 'add-node-modules-path)
+(eval-after-load 'js-mode
+  '(add-hook 'js-mode-hook #'add-node-modules-path))
