94a84cb40f3582da0859412427b7c82e8324d02f
Mohit Thatte <mohit@pitch.io>
2021-01-13 13:09:26 +0100

    Custom stuff
    - why am I still commiting this!

--- personal/custom.el
+++ personal/custom.el
@@ -4,7 +4,18 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(plantuml-mode plantuml wgrep-helm helm-ag-r add-node-modules-path rjsx rjsx-mode company json-mode yaml-mode zop-to-char zenburn-theme which-key volatile-highlights undo-tree super-save smartrep smartparens operate-on-number move-text markdown-mode magit imenu-anywhere hl-todo helm-projectile helm-descbinds helm-ag guru-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region exec-path-from-shell elm-mode editorconfig easy-kill discover-my-major diminish diff-hl crux clj-refactor browse-kill-ring beacon anzu ag ace-window)))
+   '(stylus-mode plantuml-mode plantuml wgrep-helm helm-ag-r add-node-modules-path rjsx rjsx-mode company json-mode yaml-mode zop-to-char zenburn-theme which-key volatile-highlights undo-tree super-save smartrep smartparens operate-on-number move-text markdown-mode magit imenu-anywhere hl-todo helm-projectile helm-descbinds helm-ag guru-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region exec-path-from-shell elm-mode editorconfig easy-kill discover-my-major diminish diff-hl crux clj-refactor browse-kill-ring beacon anzu ag ace-window))
+ '(safe-local-variable-values
+   '((eval define-clojure-indent
+           (reg-cofx :defn)
+           (reg-event-db :defn)
+           (reg-event-fx :defn)
+           (reg-fx :defn)
+           (reg-sub :defn)
+           (reg-event-domain :defn)
+           (reg-block-event-fx :defn)
+           (reg-event-domain-fx :defn)
+           (this-as 0)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
