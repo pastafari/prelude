45b1a8703364efa1027c8c0a9d1bcf2d0cb57495
Mohit Thatte <mohit@pitch.io>
2021-07-02 11:34:28 +0200

    Add lsp-mode and clojure settings

--- personal/custom.el
+++ personal/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(stylus-mode plantuml-mode plantuml wgrep-helm helm-ag-r add-node-modules-path rjsx rjsx-mode company json-mode yaml-mode zop-to-char zenburn-theme which-key volatile-highlights undo-tree super-save smartrep smartparens operate-on-number move-text markdown-mode magit imenu-anywhere hl-todo helm-projectile helm-descbinds helm-ag guru-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region exec-path-from-shell elm-mode editorconfig easy-kill discover-my-major diminish diff-hl crux clj-refactor browse-kill-ring beacon anzu ag ace-window))
+   '(lsp-treemacs lsp-mode svg stylus-mode plantuml-mode plantuml wgrep-helm helm-ag-r add-node-modules-path rjsx rjsx-mode company json-mode yaml-mode zop-to-char zenburn-theme which-key volatile-highlights undo-tree super-save smartrep smartparens operate-on-number move-text markdown-mode magit imenu-anywhere hl-todo helm-projectile helm-descbinds helm-ag guru-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region exec-path-from-shell elm-mode editorconfig easy-kill discover-my-major diminish diff-hl crux clj-refactor browse-kill-ring beacon anzu ag ace-window))
  '(safe-local-variable-values
    '((eval define-clojure-indent
            (reg-cofx :defn)
