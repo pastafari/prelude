c364161590ef7c4826615f3e0e70853bb7919d0d
Mohit Thatte <mohit@pitch.io>
2021-10-22 15:52:43 +0200

    Custom updated

--- personal/custom.el
+++ personal/custom.el
@@ -4,9 +4,11 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(lsp-treemacs lsp-mode svg stylus-mode plantuml-mode plantuml wgrep-helm helm-ag-r add-node-modules-path rjsx rjsx-mode company json-mode yaml-mode zop-to-char zenburn-theme which-key volatile-highlights undo-tree super-save smartrep smartparens operate-on-number move-text markdown-mode magit imenu-anywhere hl-todo helm-projectile helm-descbinds helm-ag guru-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region exec-path-from-shell elm-mode editorconfig easy-kill discover-my-major diminish diff-hl crux clj-refactor browse-kill-ring beacon anzu ag ace-window))
+   '(company-terraform terraform-doc terraform-mode cljr-helm cider lsp-treemacs lsp-mode svg stylus-mode plantuml-mode plantuml wgrep-helm helm-ag-r add-node-modules-path rjsx rjsx-mode company json-mode yaml-mode zop-to-char zenburn-theme which-key volatile-highlights undo-tree super-save smartrep smartparens operate-on-number move-text markdown-mode magit imenu-anywhere hl-todo helm-projectile helm-descbinds helm-ag guru-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region exec-path-from-shell elm-mode editorconfig easy-kill discover-my-major diminish diff-hl crux clj-refactor browse-kill-ring beacon anzu ag ace-window))
  '(safe-local-variable-values
-   '((eval define-clojure-indent
+   '((cider-shadow-default-options . "app")
+     (cider-default-cljs-repl . shadow)
+     (eval define-clojure-indent
            (reg-cofx :defn)
            (reg-event-db :defn)
            (reg-event-fx :defn)
