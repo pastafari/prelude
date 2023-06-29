991921d765116b7d7eef98bd630cbaca94bba676
Mohit Thatte <pastafari@Mohits-MBP.fritz.box>
2023-06-29 18:33:19 +0200

    require prelude-rust

--- personal/custom.el
+++ personal/custom.el
@@ -6,7 +6,20 @@
  '(custom-safe-themes
    '("18cf5d20a45ea1dff2e2ffd6fbcd15082f9aa9705011a3929e77129a971d1cb3" "3f1dcd824a683e0ab194b3a1daac18a923eed4dba5269eecb050c718ab4d5a26" default))
  '(package-selected-packages
-   '(darcula-theme flycheck-clj-kondo bazel wgrep-helm plantuml-mode lsp-treemacs add-node-modules-path rjsx-mode clj-refactor cider clojure-mode helm-ag helm-descbinds helm-projectile helm yaml-mode web-mode lsp-ui lsp-mode json-mode js2-mode rainbow-mode elisp-slime-nav rainbow-delimiters company consult orderless vertico exec-path-from-shell zop-to-char zenburn-theme which-key volatile-highlights undo-tree super-save smartrep smartparens operate-on-number nlinum move-text magit projectile imenu-anywhere hl-todo guru-mode git-modes git-timemachine gist flycheck expand-region epl editorconfig easy-kill diminish diff-hl discover-my-major crux browse-kill-ring anzu ag ace-window)))
+   '(rust-mode company-terraform terraform-doc terraform-mode cljr-helm cider lsp-treemacs lsp-mode svg stylus-mode plantuml-mode plantuml wgrep-helm helm-ag-r add-node-modules-path rjsx rjsx-mode company json-mode yaml-mode zop-to-char zenburn-theme which-key volatile-highlights undo-tree super-save smartrep smartparens operate-on-number move-text markdown-mode magit imenu-anywhere hl-todo helm-projectile helm-descbinds helm-ag guru-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region exec-path-from-shell elm-mode editorconfig easy-kill discover-my-major diminish diff-hl crux clj-refactor browse-kill-ring beacon anzu ag ace-window))
+ '(safe-local-variable-values
+   '((cider-shadow-default-options . "app")
+     (cider-default-cljs-repl . shadow)
+     (eval define-clojure-indent
+           (reg-cofx :defn)
+           (reg-event-db :defn)
+           (reg-event-fx :defn)
+           (reg-fx :defn)
+           (reg-sub :defn)
+           (reg-event-domain :defn)
+           (reg-block-event-fx :defn)
+           (reg-event-domain-fx :defn)
+           (this-as 0)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
