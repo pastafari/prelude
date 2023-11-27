f7cd8eff87f050bd1b6bc10532b30ab8d6f75b60
Mohit Thatte <mohit@griffin.com>
2023-11-27 13:09:40 +0100

    custom changed

--- personal/custom.el
+++ personal/custom.el
@@ -4,23 +4,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(company-terraform terraform-doc terraform-mode cljr-helm cider lsp-treemacs lsp-mode svg stylus-mode plantuml-mode plantuml wgrep-helm helm-ag-r add-node-modules-path rjsx rjsx-mode company json-mode yaml-mode zop-to-char zenburn-theme which-key volatile-highlights undo-tree super-save smartrep smartparens operate-on-number move-text markdown-mode magit imenu-anywhere hl-todo helm-projectile helm-descbinds helm-ag guru-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region exec-path-from-shell elm-mode editorconfig easy-kill discover-my-major diminish diff-hl crux clj-refactor browse-kill-ring beacon anzu ag ace-window))
- '(safe-local-variable-values
-   '((cider-shadow-default-options . "app")
-     (cider-default-cljs-repl . shadow)
-     (eval define-clojure-indent
-           (reg-cofx :defn)
-           (reg-event-db :defn)
-           (reg-event-fx :defn)
-           (reg-fx :defn)
-           (reg-sub :defn)
-           (reg-event-domain :defn)
-           (reg-block-event-fx :defn)
-           (reg-event-domain-fx :defn)
-           (this-as 0)))))
+   '(bazel wgrep-helm plantuml-mode lsp-treemacs add-node-modules-path rjsx-mode clj-refactor cider clojure-mode helm-ag helm-descbinds helm-projectile helm yaml-mode web-mode lsp-ui lsp-mode json-mode js2-mode rainbow-mode elisp-slime-nav rainbow-delimiters company consult orderless vertico exec-path-from-shell zop-to-char zenburn-theme which-key volatile-highlights undo-tree super-save smartrep smartparens operate-on-number nlinum move-text magit projectile imenu-anywhere hl-todo guru-mode git-modes git-timemachine gist flycheck expand-region epl editorconfig easy-kill diminish diff-hl discover-my-major crux browse-kill-ring anzu ag ace-window)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#3F3F3F" :foreground "#DCDCCC" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 160 :width normal :foundry "nil" :family "Monaco")))))
+ )
